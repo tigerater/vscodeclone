8d9131f63eda980479f827138d1243abe6f751fb
tigerater <tigerater@gmail.com>
2020-03-08 23:48:18 +0000

    1575

--- test/assert.js
+++ test/assert.js
@@ -468,10 +468,7 @@ assert.doesNotThrow = function(block, /*optional*/message) {
   _throws.apply(this, [false].concat(pSlice.call(arguments)));
 };
 
-// VSCODE-CHANGE, todo@joh this isn't correct!!
-assert.deepStrictEqual = assert.deepEqual;
-
-assert.ifError = function (err) { if (err) { throw err; } };
+assert.ifError = function(err) { if (err) {throw err;}};
 
 return assert;
 });
